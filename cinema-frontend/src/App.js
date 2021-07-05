7d6697119ce0a59ff0f15bf5bf1108c4487defc3
Amandeep Bassi <asbassi@outlook.com>
2021-07-05 15:39:40 +0100

    QC-1 Created simple nav bar and application routing for the different pages

--- cinema-frontend/src/App.js
+++ cinema-frontend/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/listing">
+
+        </Route>
+        <Route path="/upcoming">
+
+        </Route>
+        <Route path="/bookings">
+
+        </Route>
+        <Route path="/openingtimes">
+
+        </Route>
+        <Route path="/classifications">
+
+        </Route>
+        <Route path="/gettingthere">
+
+        </Route>
+        <Route path="/placestogo">
+
+        </Route>
+        <Route path="/discussion">
+
+        </Route>
+      </Switch>
+    </Router >
   );
 }
 
