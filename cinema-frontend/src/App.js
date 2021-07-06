92d90260dd9c345a3a13df0f63017f8cfae7c84e
Scott Stevens <scott.stevens404@yahoo.com>
2021-07-06 14:38:34 +0100

    Added basic footer

--- cinema-frontend/src/App.js
+++ cinema-frontend/src/App.js
@@ -8,48 +8,53 @@ import Screens from './components/Screens';
 import Opening from './components/Opening';
 import AboutUs from './components/AboutUs';
 import Classifications from './components/Classifications';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/listing">
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/listing">
 
-        </Route>
-        <Route path="/upcoming">
+          </Route>
+          <Route path="/upcoming">
 
-        </Route>
-        <Route path="/bookings">
+          </Route>
+          <Route path="/bookings">
 
-        </Route>
-        <Route path="/openingtimes">
-          <Opening />
-        </Route>
-        <Route path="/classifications">
-          <Classifications />
-        </Route>
-        <Route path="/gettingthere">
-          <GettingThere />
-        </Route>
-        <Route path="/placestogo">
+          </Route>
+          <Route path="/openingtimes">
+            <Opening />
+          </Route>
+          <Route path="/classifications">
+            <Classifications />
+          </Route>
+          <Route path="/gettingthere">
+            <GettingThere />
+          </Route>
+          <Route path="/placestogo">
 
-        </Route>
-        <Route path="/discussion">
+          </Route>
+          <Route path="/discussion">
 
-        </Route>
-        <Route path="/screens">
-          <Screens />
-        </Route>
-        <Route path="/aboutus">
-          <AboutUs />
-    
-        </Route>
-      </Switch>
-    </Router >
+          </Route>
+          <Route path="/screens">
+            <Screens />
+          </Route>
+          <Route path="/aboutus">
+            <AboutUs />
+
+          </Route>
+        </Switch>
+      </Router >
+
+      <Footer />
+    </div>
   );
 }
 
