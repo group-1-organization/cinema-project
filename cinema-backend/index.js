1559a1722f0cff63cfd660638f5bae181188b0cb
Amandeep Bassi <asbassi@outlook.com>
2021-07-07 09:24:15 +0100

    QC-11 Created simple schema for topic in backend with create and read functions

--- cinema-backend/index.js
+++ cinema-backend/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const movieroutes = require('./routes/movies');
 const bookingroutes = require('./routes/bookings');
+const topicroutes = require('./routes/topics');
 
 mongoose.connect('mongodb://localhost/cinema', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://localhost/cinema', { useNewUrlParser: true, useUnifi
         app.use(express.json());
         app.use('/cinema', movieroutes);
         app.use('/cinema', bookingroutes);
+        app.use('/cinema', topicroutes);
         app.listen(5000, () => {
             console.log("server has started");
         });
---
+++ cinema-backend/routes/topics.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+const Topic = require('../models/Topic');
+
+//create topic
+router.post('/topics', async (req, res) => {
+    try {
+        const topic = new Topic({
+            name: req.body.name,
+            movie: req.body.movie,
+            author: req.body.author
+        });
+        console.log("Log: " + topic);
+        await topic.save();
+        res.send(topic);
+    } catch {
+        res.status(400);
+        res.send({ error: "bad request" });
+    }
+})
+
+//get all topics
+router.get('/topics', async (req, res) => {
+    try {
+        const topic = await Topic.find();
+        res.send(topic);
+    } catch {
+        res.status(404);
+        res.send({ error: "topic doesn't exist" });
+    }
+})
+
+//find topic
+router.get('/topics/:id', async (req, res) => {
+    try {
+        const topic = await Topic.findById(req.params.id);
+        res.send(topic);
+    } catch {
+        res.status(404);
+        res.send({ error: "topic doesn't exist" });
+    }
+})
+
+module.exports = router;
