693d7f85eba3543712fd882b8c4435f951ab2e8d
Jamie Barnett <jamiebarnett44@gmail.com>
2021-07-14 12:43:08 +0100

    QC-31 JB: Implemented update and delete for topics, with tests

--- cinema-backend/_tests_/topics.test.js
+++ cinema-backend/_tests_/topics.test.js
@@ -8,8 +8,28 @@ const Topic = require('../models/Topic');
 
 describe(`Topics testing`, () => {
 
+    before(async () => {
+        let deleteTest = Topic({
+            "name": "test topic",
+            "movieName": "testmovie",
+            "author": "deleteTest",
+        })
+
+        let updateTest = Topic({
+            "name": "test topic",
+            "movieName": "testmovie",
+            "author": "updateTest",
+        })
+        await deleteTest.save();
+        await updateTest.save();
+    })
+
     after(() => {
-        Topic.deleteMany({ "name": "test topic" }, function (err) {
+        Topic.deleteMany({ 'author': 'createTest' }, function (err) {
+            if (err) return handleError(err);
+        });
+
+        Topic.deleteMany({ 'author': 'updateTest' }, function (err) {
             if (err) return handleError(err);
         });
     });
@@ -18,7 +38,7 @@ describe(`Topics testing`, () => {
         chai.request(app).post('/cinema/topics').send({
             "name": "test topic",
             "movieName": "testmovie",
-            "author": "James",
+            "author": "createTest",
             "createdAt": "2021-07-12T12:30:46.369Z",
             "updatedAt": "2021-07-12T12:30:46.369Z",
         }).end((error, response) => {
@@ -68,5 +88,53 @@ describe(`Topics testing`, () => {
             expect(topic.name).to.be.a("string");
         })
         done();
+    })
+
+    it(`should update the topic in the db`, (done) => {
+        Topic.findOne(
+            { 'author': 'updateTest' }, (err, topic) => {
+                if (err) {
+                    console.error('An error occurred:', err);
+                } else {
+                    chai.request(app).patch(`/cinema/topics/${topic._id}`).send({ "name": "the name has been updated" }).end((error, response) => {
+                        if (error) {
+                            console.log(`Something went wrong`);
+                            done(error);
+                        }
+                        expect(response).to.have.status(200);
+                        expect(response).to.not.be.null;
+                        const topic = response.body;
+                        console.log(topic.text);
+                        expect(topic).to.be.a("object");
+                        expect(topic).to.contain.keys("author");
+                        expect(topic.author).to.be.a("string");
+                        expect(topic.author).to.equal("updateTest");
+                        expect(topic).to.contain.keys("name");
+                        expect(topic.name).to.be.a("string");
+                        expect(topic.name).to.equal("the name has been updated");
+                        done();
+                    })
+                }
+            })
+    })
+
+    it(`should delete the topic in the db`, (done) => {
+        Topic.findOne(
+            { 'author': 'deleteTest' }, (err, topic) => {
+                if (err) {
+                    console.error('An error occurred:', err);
+                } else {
+                    chai.request(app).delete(`/cinema/topics/${topic._id}`).end((error, response) => {
+                        if (error) {
+                            console.log(`Something went wrong`);
+                            done(error);
+                        }
+                        expect(response).to.have.status(200);
+                        expect(response.text).to.equal(`Topic ${topic.name} created by ${topic.author} at ${topic.createdAt} deleted`);
+                        expect(response).to.not.be.null;
+                        done();
+                    })
+                }
+            })
     })
 })
